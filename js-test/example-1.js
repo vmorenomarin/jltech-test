98f319d1f6af6d67a1ce96faa0ba9c4037983292
vmorenomarin <vmorenomarin@gmail.com>
2022-01-26 01:09:48 -0500

    First exersice solved.

--- js-test/example-1.js
+++ js-test/example-1.js
@@ -3,7 +3,84 @@ const companies = createAll();
 
 cleanConsole(1, companies);
 
-console.log("----SOLUTION EXAMPLE 1 --- ", companies);
+const getUsers = (companies) => {
+  const users = [];
+  companies.map((companie) => {
+    users.push(companie["users"]);
+  });
+  return users;
+};
+
+const users = getUsers(companies);
+// console.log(users);
+
+// Next function replace "undefined" value in "firstName" and "lastName" keys with "" character value also, returns all firstname and lastnames with uppercase at first letter.
+
+const changeUndefined = (users) => {
+  // Replacing and uppercase first letter
+  for (let i = 0; i < users.length; i++) {
+    users[i].map((user) => {
+      user["firstName"] === undefined
+        ? (user["firstName"] = "")
+        : (user["firstName"] =
+            user["firstName"][0].toUpperCase() + user["firstName"].slice(1));
+      user["lastName"] === undefined
+        ? (user["lastName"] = "")
+        : (user["lastName"] =
+            user["lastName"][0].toUpperCase() + user["lastName"].slice(1));
+    });
+
+    // Sorting user names
+    users[i].sort((user1, user2) => {
+      if (user1["firstName"] > user2["firstName"]) {
+        return 1;
+      }
+      if (user1["firstName"] < user2["firstName"]) {
+        return -1;
+      }
+      if (user1["lastName"] < user2["lastName"]) {
+        return -1;
+      }
+      return 0;
+    });
+  }
+  return users;
+};
+
+const getCompaniesName = (companies) => {
+  const companiesName = [];
+  companies.map((companie) => {
+    companiesName.push(
+      companie["name"][0].toUpperCase() + companie["name"].slice(1)
+    );
+  });
+  return companiesName;
+};
+
+const assamblingData = (companiesName, users, companies) => {
+  for (let j = 0; j < companies.length; j++) {
+    for (let i = 0; i < users[j].length; i++) {
+      companies[j]["users"][i]["firstName"] = users[j][i]["firstName"];
+      companies[j]["users"][i]["lastName"] = users[j][i]["lastName"];
+    }
+    companies[j]["name"] = companiesName[j];
+  }
+  companies.sort((companie1, companie2) => {
+    if (companie1.users.length > companie2.users.length) {
+      return -1;
+    }
+    if (companie1.users.length < companie2.users.length) {
+      return 1;
+    }
+    return 0;
+  });
+
+  return companies;
+};
+
+let usersModified = changeUndefined(users);
+
+console.log("----SOLUTION EXAMPLE 1 --- ", assamblingData(getCompaniesName(companies), usersModified, companies));
 
 // -----------------------------------------------------------------------------
 // INSTRUCCIONES EN ESPAÑOL
