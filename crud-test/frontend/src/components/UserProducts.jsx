5c88a1916282033bd0bc047845748eb0b819aa72
vmorenomarin <vmorenomarin@gmail.com>
2022-06-02 22:51:22 -0500

    Added modal window to edit user product.

--- crud-test/frontend/src/components/UserProducts.jsx
+++ crud-test/frontend/src/components/UserProducts.jsx
@@ -1,52 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import moment from "moment";
+import { Modal } from "react-bootstrap";
 
 export const UserProducts = (props) => {
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
   return (
-    <div id="userProduct" className="row">
+    <div id="userProduct" className="container mt-3">
       <h1 className="display-6">My Products</h1>
-      <div className="container d-flex flex-row rounded bg-white p-3">
+      <div className="row row-cols-2 justify-content-evenly">
         {props.products.map((product) => (
-          <div className="card col-3 mx-2" key={product._id}>
-            <div className="card-header">
-              <h6>{product.name}</h6>
-              <span className="small">{product.code}</span>
-            </div>
-            <img
-              src={product.img}
-              alt={product.name}
-              className="card-img-top"
-            />
-            <div className="card-body">
-              <span className="fw-bold">Updated:</span>
-              <p className="small">
-                {moment(product.updatedAt).format("L H:mm:ss")}
-              </p>
-              <span className="fw-bold">Created:</span>
-              <p className="small">
-                {moment(product.createdAt).format("L H:mm:ss")}
-              </p>
-            </div>
-            <div className="card-footer d-flex flex-row justify-content-evenly">
-              <button
-                className="btn btn-sm btn-primary"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-                title="Edit product"
-              >
-                <i className="fa fa-pen"></i>
-              </button>
-              <button
-                className="btn btn-sm btn-danger"
-                data-bs-toggle="tooltip"
-                data-bs-placement="top"
-                title="Delete product"
-              >
-                <i className="fa fa-trash"></i>
-              </button>
+          <div className="col-md-3 col-sm-4 m-2 p-0" key={product._id}>
+            <div className="card card-product">
+              <img
+                src={product.img}
+                alt={product.name}
+                className="card-img-top"
+              />
+              <div className="card-body">
+                <div className="card-title">
+                  <h6>{product.name}</h6>
+                  <span className="small">{product.code}</span>
+                </div>
+                <span className="fw-bold">Updated:</span>
+                <p className="small">
+                  {moment(product.updatedAt).format("L H:mm:ss")}
+                </p>
+                <span className="fw-bold">Created:</span>
+                <p className="small">
+                  {moment(product.createdAt).format("L H:mm:ss")}
+                </p>
+              </div>
+              <div className="d-flex flex-row justify-content-evenly mb-2">
+                <button
+                  type="button"
+                  className="btn btn-sm btn-primary"
+                  data-bs-toggle="tooltip"
+                  data-bs-placement="top"
+                  title="Edit product"
+                  onClick={handleShow}
+                >
+                  <i className="fa fa-pen"></i>
+                </button>
+                <button
+                  className="btn btn-sm btn-danger"
+                  data-bs-toggle="tooltip"
+                  data-bs-placement="top"
+                  title="Delete product"
+                >
+                  <i className="fa fa-trash"></i>
+                </button>
+              </div>
             </div>
           </div>
-        ))}
+        ))}    
+
+        <div>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Edit product data</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Text Hi</Modal.Body>
+          </Modal>
+        </div>
       </div>
     </div>
   );
