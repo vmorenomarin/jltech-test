3b3281e54a190f4a66c9f7174ea2484639db1b15
vmorenomarin <vmorenomarin@gmail.com>
2022-06-08 00:11:10 -0500

    Enhanced edir product Modal window.

--- crud-test/frontend/src/components/UserProducts.jsx
+++ crud-test/frontend/src/components/UserProducts.jsx
@@ -1,16 +1,26 @@
 import React, { useState } from "react";
 import moment from "moment";
-import { Modal } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 
 export const UserProducts = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [product, setProduct] = useState();
   const [show, setShow] = useState(false);
   const handleShow = () => setShow(true);
   const handleClose = () => setShow(false);
+
+  const updateProduct = async (productToUpdate) => {
+    try {
+      setLoading(true);
+    } catch (error) {}
+  };
+
   return (
     <div id="userProduct" className="container mt-3">
       <h1 className="display-6">My Products</h1>
       <div className="row row-cols-2 justify-content-evenly">
         {props.products.map((product) => (
+          // console.log(product),
           <div className="col-md-3 col-sm-4 m-2 p-0" key={product._id}>
             <div className="card card-product">
               <img
@@ -39,7 +49,10 @@ export const UserProducts = (props) => {
                   data-bs-toggle="tooltip"
                   data-bs-placement="top"
                   title="Edit product"
-                  onClick={handleShow}
+                  onClick={() => {
+                    handleShow();
+                    setProduct(product);
+                  }}
                 >
                   <i className="fa fa-pen"></i>
                 </button>
@@ -54,16 +67,68 @@ export const UserProducts = (props) => {
               </div>
             </div>
           </div>
-        ))}    
-
-        <div>
-          <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Edit product data</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Text Hi</Modal.Body>
-          </Modal>
-        </div>
+        ))}
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit product data </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={(e) => updateProduct(product)}>
+              <Form.Group controlId="productName">
+                <Form.Label className="fw-bold">Product Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={product.name}
+                  onChange={(e) =>
+                    setProduct({ ...product, name: e.target.value })
+                  }
+                />
+              </Form.Group>
+              <Form.Group controlId="productPrice">
+                <Form.Label className="fw-bold">Product Price</Form.Label>
+                <Form.Control
+                  type="number"
+                  value={product.price}
+                  onChange={(e) =>
+                    setProduct({ ...product, price: e.target.value })
+                  }
+                />
+              </Form.Group>
+              <Form.Group controlId="productCategory">
+                <Form.Label className="fw-bold">Product Category</Form.Label>
+                <Form.Select
+                  type="text"
+                  value={product.category}
+                  onChange={(e) =>
+                    setProduct({ ...product, category: e.target.value })
+                  }
+                >
+                  <option>Confitería</option>
+                  <option>Aseo y Cuidado Personal</option>
+                  <option>Tecnología</option>
+                </Form.Select>
+              </Form.Group>
+              <Form.Group controlId="productStock">
+                <Form.Label className="fw-bold">Product Category</Form.Label>
+                <Form.Control
+                  disabled
+                  type="number"
+                  value={product.stock}
+                ></Form.Control>
+              </Form.Group>
+              <div className="d-flex justify-content-end">
+                <Button
+                  variant="primary"
+                  onClick={handleClose}
+                  type="submit"
+                  className="mt-2 "
+                >
+                  <i className="fa fa-save me-1"></i> Save Changes
+                </Button>
+              </div>
+            </Form>
+          </Modal.Body>
+        </Modal>
       </div>
     </div>
   );
